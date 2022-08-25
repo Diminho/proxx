f46b2a059f111007705fbfd296b82ac0bad30583
Dmytro Ukolov <dmytro.ukolov@connectwise.com>
2022-08-25 21:22:01 +0300

    silence usage for root

--- cmd/root.go
+++ cmd/root.go
@@ -7,11 +7,9 @@ import (
 // Execute executes proxx
 func Execute() error {
 	command := &cobra.Command{
-		Use:   "proxx",
-		Short: "Proxx game",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return cmd.Usage()
-		},
+		Use:          "proxx",
+		Short:        "Proxx game",
+		SilenceUsage: true,
 	}
 
 	command.AddCommand(start())
